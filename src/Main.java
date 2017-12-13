63c8755d19cc79f816ae643faad344012a76eaed
petbir <petbir@gmail.com>
2017-12-13 12:37:05 +0200

    update main

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("first commit");
+
+        System.out.println("4 commit");
     }
 }
